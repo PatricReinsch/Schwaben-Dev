3f687172bccb58bd7eb6a655bf8eee982836f394
MaxG99 <49040380+MaxG99@users.noreply.github.com>
2024-11-23 00:10:48 +0100

    setup micronaut

---
+++ src/test/java/com/schwaben_devs/MotoMatchTest.java
@@ -0,0 +1,21 @@
+package com.schwaben_devs;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MotoMatchTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
