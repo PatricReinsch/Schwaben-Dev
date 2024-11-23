751d05b00acfc79a9baf00d3b72e006719e51f52
MaxG99 <49040380+MaxG99@users.noreply.github.com>
2024-11-23 04:57:30 +0100

    moved auto generated resources to src

--- src/main/java/com/schwaben_devs/controller/ChallengeController.java
+++ src/main/java/com/schwaben_devs/controller/ChallengeController.java
@@ -1,26 +1,46 @@
 package com.schwaben_devs.controller;
 
-import com.schwaben_devs.motomatch.api.ChallengeApi;
-import com.schwaben_devs.motomatch.model.CreateOffersRequest;
-import com.schwaben_devs.motomatch.model.GetOffers200Response;
-import com.schwaben_devs.motomatch.model.GetOffersCarTypeParameter;
-import com.schwaben_devs.motomatch.model.GetOffersSortOrderParameter;
+import com.schwaben_devs.model.CreateOffersRequest;
+import com.schwaben_devs.model.GetOffers200Response;
+import com.schwaben_devs.model.GetOffersCarTypeParameter;
+import com.schwaben_devs.model.GetOffersSortOrderParameter;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.http.annotation.*;
 
 import java.math.BigDecimal;
 
-public class ChallengeController implements ChallengeApi {
-    @Override
+@Controller("/api/offers")
+public class ChallengeController {
+    @Delete
     public void cleanupData() {
-
+        System.out.println("Cleaning up Data");
     }
 
-    @Override
-    public void createOffers(CreateOffersRequest createOffersRequest) {
-
+    @Post
+    public void createOffers(@Body CreateOffersRequest createOffersRequest
+    ) {
+        System.out.println("Creating Offers");
     }
 
-    @Override
-    public GetOffers200Response getOffers(Integer regionID, Long timeRangeStart, Long timeRangeEnd, Integer numberDays, GetOffersSortOrderParameter sortOrder, Long page, Long pageSize, Long priceRangeWidth, Long minFreeKilometerWidth, Integer minNumberSeats, BigDecimal minPrice, BigDecimal maxPrice, GetOffersCarTypeParameter carType, Boolean onlyVollkasko, Integer minFreeKilometer) {
+    @Get
+    public GetOffers200Response getOffers(
+            @QueryValue("regionID") Integer regionID,
+            @QueryValue("timeRangeStart") Long timeRangeStart,
+            @QueryValue("timeRangeEnd") Long timeRangeEnd,
+            @QueryValue("numberDays") Integer numberDays,
+            @QueryValue("sortOrder") GetOffersSortOrderParameter sortOrder,
+            @QueryValue("page") Long page,
+            @QueryValue("pageSize") Long pageSize,
+            @QueryValue("priceRangeWidth") Long priceRangeWidth,
+            @QueryValue("minFreeKilometerWidth") Long minFreeKilometerWidth,
+            @QueryValue("minNumberSeats") @Nullable(inherited = true) Integer minNumberSeats,
+            @QueryValue("minPrice") @Nullable(inherited = true) BigDecimal minPrice,
+            @QueryValue("maxPrice") @Nullable(inherited = true) BigDecimal maxPrice,
+            @QueryValue("carType") @Nullable(inherited = true) GetOffersCarTypeParameter carType,
+            @QueryValue("onlyVollkasko") @Nullable(inherited = true) Boolean onlyVollkasko,
+            @QueryValue("minFreeKilometer") @Nullable(inherited = true) Integer minFreeKilometer
+    ) {
+        System.out.println("Get Offers");
         return null;
     }
 }
